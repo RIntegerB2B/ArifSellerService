c5408b5d0860c644fccb307f8c5ef529d67ffe84
Chandran <Preveen_Chandran@Dell.com>
2018-05-28 21:12:28 +0530

    Quick Changes

--- src/server.js
+++ src/server.js
@@ -13,7 +13,14 @@ app.use(bodyParser.urlencoded({
     limit: '50mb',
     extended: true
 }));
-app.use(cors());
+let options = cors.CorsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: API_URL,
+    preflightContinue: false
+  };
+app.use(cors(options));
 routes.loadRoutes(app);
 app.listen(port);
 
