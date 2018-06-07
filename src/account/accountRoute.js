87127faf830f4648bb439ca0e24b4d3765b16081
Chandran <Preveen_Chandran@Dell.com>
2018-06-07 07:00:42 +0530

    PWD Change

--- src/account/accountRoute.js
+++ src/account/accountRoute.js
@@ -1,9 +1,16 @@
 'use strict';
 var signInMgr = require('./signIn/signInMgr');
+var pwdChangeMgr = require('./pwdChange/pwdChangeMgr');
 module.exports = function (app) {
     app.route('/admin')
         .post(signInMgr.create);
 
     app.route('/admin/validate')
         .post(signInMgr.signInToSite);
+
+    /*app.route('/pwdChange/:emailId')
+        .post(pwdChangeMgr.pwdChangeRequest);
+
+    app.route('/pwdChange/reset/:key')
+        .post(pwdChangeMgr.pwdChangeReset); */
 }
