ee7b20aa89ecb75ae4c389fe08dec56ba7f421d4
Vikasini <vikasininithi@gmail.com>
2018-06-07 18:19:14 +0530

    pwdChangeService commit

--- src/account/pwdChange/pwdChangeDA.js
+++ src/account/pwdChange/pwdChangeDA.js
@@ -1,25 +1,7 @@
-'use strict';
-var ReqToAdmin = require('../../model/adminReqPwd.model');
 
-exports.pwdChangeRequest = function (req, res) {
-    ReqToAdmin.update({
-        'randomKey':"xyz",
-
-        'validUntil':formattedData =function(req,res)
-        {
-            var currentDate = new Date();
-var numberOfDaysToAdd = 2;
-currentDate.setDate(currentDate.getDate() + numberOfDaysToAdd); 
-
-var dd = currentDate.getDate();
-var mm = currentDate.getMonth() + 1;
-var y = currentDate.getFullYear();
-
-var someFormattedDate = dd + '/'+ mm + '/'+ y;
-      return  someFormattedDate;     
-        }
-    })
-
-};
-
-
+               'use strict';
+               var pwdChangeDA = require('./pwdChangeDA');
+               exports.pwdChangeRequest = function (req, res) {
+                pwdChangeDA.pwdChangeRequest(req,res,someFormattedDate);
+               };
+               
--- src/account/pwdChange/pwdChangeMgr.js
+++ src/account/pwdChange/pwdChangeMgr.js
@@ -1,7 +1,32 @@
+'use strict';
+var ReqToAdmin = require('../../model/adminReqPwd.model');
 
-               'use strict';
-               var pwdChangeDA = require('./pwdChangeDA');
-               exports.pwdChangeRequest = function (req, res) {
-                pwdChangeDA.pwdChangeRequest(req, res);
-               };
-               
+exports.pwdChangeRequest = function (req, res,someFormattedDate) {
+    ReqToAdmin.update({
+        'randomKey':req.params.email,
+
+        'validUntil':formattedData =function(req,res)
+        {
+            var currentDate = new Date();
+ 
+var numberOfDaysToAdd = 2;
+currentDate.setDate(currentDate.getDate() + numberOfDaysToAdd); 
+
+var dd = currentDate.getDate();
+var mm = currentDate.getMonth() + 1;
+var y = currentDate.getFullYear();
+
+var someFormattedDate = dd + '/'+ mm + '/'+ y;
+     return validDate=someFormattedDate.toString();
+
+        },
+        function(err, affected, resp) 
+{
+          console.log(resp);
+          res.send(affected);
+        }
+
+    })
+
+};
+
