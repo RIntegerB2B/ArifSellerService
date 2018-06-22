e8c629cdd7b9649fd2afdafefa2c1cd24d7bd513
Chandran <Preveen_Chandran@Dell.com>
2018-06-22 22:54:18 +0530

    Product Module Folders

---
+++ src/product/productRoute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (app) {
+    /*
+    app.route('/catalog')
+        .post(signInMgr.create);
+
+    app.route('/catalog/:id')
+        .get(signInMgr.signInToSite); */
+}
