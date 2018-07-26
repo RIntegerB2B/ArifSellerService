713a001ded5fc8e34bcea0b994005f72aa9f9cc8
Chandran <Preveen_Chandran@Dell.com>
2018-07-26 12:09:32 +0530

    URL Change

---
+++ src/notification/notificationRoute.js
@@ -0,0 +1,9 @@
+'use strict';
+var notificationMgr = require('./notificationMgr');
+
+module.exports = function (app) {
+    app.route('/notification')
+        .post(notificationMgr.addPushSubscriber);
+        app.route('/bookingStatus')
+        .post(notificationMgr.bookingStatus);
+}
