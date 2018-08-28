ea4f986a1e1f1509201b8f3b9209da39d34d2276
Chandran <Preveen_Chandran@Dell.com>
2018-08-28 16:10:14 +0530

    Path

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,5 +1,3 @@
 module.exports = {
-   //   imageServerPath: 'http://localhost/Seller/'
-    imageServerPath: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:81/images/'
-
-}
+    imageServerPath: process.env.SELLER_IMAGE_SERVER_PATH || 'http://localhost/Seller/'
+};
