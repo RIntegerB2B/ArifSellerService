63ae77d0398faa9581fa285850f4c09368ed894d
Vikasini <vikasininithi@gmail.com>
2019-01-07 17:26:42 +0530

    changes in product model

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -7,7 +7,7 @@ const ProductSchema  = new mongoose.Schema({
     productTypeDesc: String,
     size: String, // (L*W) - 55cm * 44cm
     productDescription: String,
-    cod: Boolean, // Cash on delivery avaible or not
+    cod: String, // Cash on delivery avaible or not
     dispatchDesc: String, // Dispatched in 2-3 days
     watsAppDesc: String, // A complete detail about the product to share with contacts thru watsapp
     imageType: String,
